b74c60150126f0a7b86734aa0d24e306513ed6b7
Gabby D <gabby529md@gmail.com>
2025-08-26 17:10:23 +0100

    Lesson 4 - Basic TT Motor Testing
    Lesson 4 - Basic TT Motor Testing

---
+++ Lesson4_TestingTTMotor/Lesson4_TestingTTMotor.ino
@@ -0,0 +1,14 @@
+
+const int motorD4 = 4;
+const int motorD5 = 5;
+
+void setup() {
+  pinMode(motorD4, OUTPUT);
+  pinMode(motorD5, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(motorD4, LOW);
+  digitalWrite(motorD5, HIGH);
+}
